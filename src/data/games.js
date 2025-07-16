43068ab28b4e0b57ccad1b3323e1fdd26f26a8a6
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-07-16 20:30:31 +0800

    Create Data

---
+++ src/data/games.js
@@ -0,0 +1,76 @@
+const games = [
+  {
+    id: 1,
+    title: "Bloodborne",
+    description: "A hunter awakens in the plagued lands of Yharnam, where the denizens have succumbed to beasthood, in an attempt to learn the truth and how to end the plague, unbeknownst of the true horror lurking within the city.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk62h2PTD0r1rB9MFnK_eWPArp8FriPJp9cw&s"
+  },
+  {
+    id: 2,
+    title: "Sekiro: Shadows Die Twice",
+    description: "In a reimagined late 1500s Sengoku period Japan, a lone shinobi embarks on a mission to rescue his kidnapped lord and exact revenge on his enemies.",
+    image: "https://upload.wikimedia.org/wikipedia/en/6/6e/Sekiro_art.jpg"
+  },
+  {
+    id: 3,
+    title: "God of War",
+    description: "In God of War, players control Kratos, a former Greek god, as he embarks on a journey with his son Atreus in a world inspired by Norse mythology.",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg"
+  },
+  {
+    id: 4,
+    title: "The Last of Us: Part I",
+    description: "In a post-apocalyptic world, a hardened survivor named Joel is tasked with escorting a young girl named Ellie across the United States, facing various threats along the way.",
+    image: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Last_of_Us_Part_I_cover.jpg"
+  },
+   {
+    id: 5,
+    title: "The Witcher 3: Wild Hunt",
+    description: "In The Witcher 3: Wild Hunt, players control Geralt of Rivia, a monster hunter searching for his adopted daughter in a visually stunning open world filled with rich storytelling and complex characters.",
+    image: "https://upload.wikimedia.org/wikipedia/en/0/0c/Witcher_3_cover_art.jpg"
+  },
+  {
+    id: 6,
+    title: "Red Dead Redemption II",
+    description: "Red Dead Redemption II is an action-adventure game set in an open world environment, following the story of Arthur Morgan, an outlaw and member of the Van der Linde gang, as they navigate the decline of the Wild West.",
+    image: "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg"
+  },
+  {
+    id: 7,
+    title: "Max Payne 2: The Fall of Max Payne",
+    description: "Max Payne 2: The Fall of Max Payne is a neo-noir third-person shooter video game developed by Remedy Entertainment and published by Rockstar Games. It is the sequel to the original Max Payne and follows the story of the titular character as he navigates a dark and gritty narrative filled with betrayal and violence.",
+    image: "https://upload.wikimedia.org/wikipedia/en/2/21/Max_Payne_2.jpg"
+  },
+  {
+    id: 8,
+    title: "Dead Space 2",
+    description: "Dead Space 2 is a survival horror video game developed by Visceral Games and published by Electronic Arts. It is the sequel to the original Dead Space and follows the story of Isaac Clarke as he battles necromorphs and uncovers the truth behind the Marker.",
+    image: "https://upload.wikimedia.org/wikipedia/en/0/0c/Dead_Space_2_Box_Art.jpg"
+  },
+  {
+    id: 9,
+    title: "The Elder Scrolls V: Skyrim",
+    description: "The Elder Scrolls V: Skyrim is an open-world action role-playing game set in a fantasy world filled with dragons, magic, and epic quests. Players assume the role of the Dragonborn, a hero prophesied to save the world from a dragon apocalypse.",
+    image: "https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png"
+  },
+  {
+    id: 10,
+    title: "BioShock Infinite",
+    description: "BioShock Infinite is a first-person shooter video game developed by Irrational Games and published by 2K Games. Set in 1912, players control Booker DeWitt as he attempts to rescue a mysterious woman named Elizabeth from the flying city of Columbia.",
+    image: "https://upload.wikimedia.org/wikipedia/en/6/6d/BioShock_cover.jpg"
+  },
+  {
+    id: 11,
+    title: "Castlevania: Symphony of the Night",
+    description: "Castlevania: Symphony of the Night is a critically acclaimed action-adventure game that follows the vampire hunter Alucard as he explores Dracula's castle, battling monsters and uncovering secrets.",
+    image: "https://upload.wikimedia.org/wikipedia/en/c/cf/Castlevania_SOTN_PAL.jpg"
+  },
+  {
+    id: 12,
+    title: "Mass Effect 2",
+    description: "Mass Effect 2 is a science fiction action role-playing game developed by BioWare and published by Electronic Arts. Players control Commander Shepard as they assemble a team to save the galaxy from a mysterious alien race known as the Collectors.",
+    image: "https://upload.wikimedia.org/wikipedia/en/0/05/MassEffect2_cover.PNG"
+  },
+];
+
+export default games;
