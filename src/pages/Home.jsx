1d8f63ac32973284f77012fd690d7a9284638d6d
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-07-17 21:40:47 +0800

    18. Style landing page buttons and text with Tailwind utilities and custom hover color for join

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
+import landingBg from "../assets/landingBg.jpg";
+import play from "../assets/play.svg";
+import crown from "../assets/crown.svg";
+
+
 export function Home() {
   return (
     <main className="home-main">
-      <h2 className="home-title">Welcome to GameVault</h2>
-      <p className="home-description">Discover and enjoy your favorite games.</p>
+      <div className="home-landing-image">
+        <img src={landingBg} alt="GameVault" className="home-landing-image" />
+      </div>
+
+      <div className="home-landing-con">
+        <div className="landing-title">Your Ultimate 
+          <span className="landing-title-span"> Gaming Library</span>
+        </div>
+        <div className="landing-text">Discover, collect, and play thousands of games across all genres. From indie gems to AAA blockbusters, GameVault has everything you need.</div>
+
+        <div className="landing-buttons-con">
+          <button className="landing-button-explore-list">
+            <img src={play} alt="Play" className="landing-button-icon" />
+            Explore Library
+          </button>
+
+          <button className="landing-button-join-list">
+            <img src={crown} alt="Join" className="landing-button-icon-join" />
+            Join Premium
+          </button>
+        </div>
+      </div>
+
     </main>
   );
 }
