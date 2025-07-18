30573c66084bd920a830a670cb68b86de05e0c01
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-07-18 21:44:35 +0800

    28. update featured-games-genre section
    - Added featured-games-genre component
    - Adjusted font styles and text color for better readability
    - Implemented 'View Game' and 'Add to Cart' buttons

--- src/components/FeaturedGamesGrid.jsx
+++ src/components/FeaturedGamesGrid.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import games from '../data/games.js';
+import cart from '../assets/cart.svg';
+import star from '../assets/star.svg';
 
 
 export function FeaturedGamesGrid() {
@@ -28,11 +30,19 @@ export function FeaturedGamesGrid() {
                 <img src={game.image} alt={game.title} className='featured-game-image-file' />
               </div>
 
-                <div className="featured-games-title">{game.title}</div>
-                <div className="featured-games-genre">{game.genre}</div>
-                <div className="featured-games-text">{game.description}</div>
+              <div className="featured-games-title" title={game.title}>{game.title}</div>
+              <div className="featured-games-genre">{game.genre}</div>
 
-             
+              <div className="featured-games-buttons-con">
+                <button className="featured-game-view-game">
+                  <img src={star} alt={game.title} className='featured-game-about' />View Game
+                </button>
+
+                <button className="featured-game-add-game">
+                  <img src={cart} alt={game.title} className='featured-game-add' /> Add to Cart
+                </button>
+              </div>
+
             </div>
           ))}
           </div>
