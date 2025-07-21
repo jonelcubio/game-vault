77e0609e7f11411e4979ab9c2690bd38077372c2
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-07-21 22:07:17 +0800

    36. Create gameLibraryImages.js in data folder
    - Added initial game data
    - Some info incomplete due to slow internet

---
+++ src/data/gameLibraryImages.js
@@ -0,0 +1,52 @@
+const gameLibraryImages = [
+  {
+    id:1,
+    title: 'Cyber Legends',
+    img: 'https://images.pexels.com/photos/4389742/pexels-photo-4389742.jpeg'
+}, {
+    id:2,
+    title: 'Medieval Realms',
+    img: 'https://images.pexels.com/photos/12978861/pexels-photo-12978861.jpeg'
+}, {
+    id:3,
+    title: 'Stellar Odyssey',
+    img: 'https://images.pexels.com/photos/7170770/pexels-photo-7170770.jpeg'
+}, {
+    id:4,
+    title: 'Speed Racer',
+    img: 'https://images.pexels.com/photos/33074676/pexels-photo-33074676.jpeg'
+}, {
+    id:5,
+    title: 'Dark Mansion',
+    img: 'https://images.pexels.com/photos/3195642/pexels-photo-3195642.jpeg'
+}, {
+    id:6,
+    title: 'Puzzle Master',
+    img:
+}, {
+    id:7,
+    title: 'Retro Arcade',
+    img:
+}, {
+    id:8,
+    title: 'Battle Strategy',
+    img:
+}, {
+    id:9,
+    title: 'Fantasy Quest',
+    img:
+}, {
+    id:10,
+    title: 'Soccer Champions',
+    img:
+}, {
+    id:11,
+    title: 'City Builder',
+    img:
+}, {
+     id:12,
+    title: 'Stay Behind',
+    img:
+}];
+
+export default gameLibraryImages;
